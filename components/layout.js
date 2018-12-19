53db782924a947b5187d5be29706830ae75eb04c
Dreamland <showd0wn.land@gmail.com>
2018-12-19 18:53:49 +0800

    up

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import { Layout } from 'antd'
+
+const { Header, Content } = Layout
 
 export default ({ children, title = '卿志宇的关注' }) => (
   <div>
@@ -10,6 +13,44 @@ export default ({ children, title = '卿志宇的关注' }) => (
       <link rel="shortcut icon" href="/static/favicon.ico" />
       <title>{ title }</title>
     </Head>
-    { children }
+    <Header></Header>
+    <Content>
+      <div className="container">
+        { children }
+      </div>
+    </Content>
+    <style global jsx>{`
+      body {
+        margin: 0;
+        padding: 0;
+      }
+      .ant-layout-header {
+        position: fixed;
+        top: 0;
+        height: 52px;
+        width: 100%;
+        background: #fff;
+        border-bottom: 1px solid #e8e8e8;
+      }
+      .ant-layout-content {
+        padding-top: 52px;
+        min-height: 100vh;
+        background: #f5f5f5;
+      }
+      .ant-list-item-meta-description {
+        overflow: hidden;
+        white-space: normal;
+        word-wrap: break-word;
+        word-break: break-all;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+      }
+      .container {
+        max-width: 960px;
+        padding: 12px;
+        margin: 0 auto;
+      }
+    `}</style>
   </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import 'isomorphic-unfetch'
+import { List, Tag } from 'antd'
 import React from 'react'
-import { Button } from 'antd'
-
 import Layout from '../components/layout'
 
+const CheckableTag = Tag.CheckableTag
+const menuMap = {
+  shh: '湿乎乎',
+  bxj: '步行街',
+  nga: 'NGA',
+  v2ex: 'V2ex',
+  zhihu: '知乎',
+  douban: '豆瓣',
+  github: 'GitHub',
+  segmentfault: 'SegmentFault',
+  wallstreetcn: '华尔街见闻',
+}
 export default class Index extends React.Component {
   static async getInitialProps() {
     // eslint-disable-next-line no-undef
@@ -12,18 +23,53 @@ export default class Index extends React.Component {
     return json.data
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      index: 0,
+      menus: Object.keys(props).filter(item => item !== 'url'),
+    }
+  }
+
   render() {
+    const sitesMenu = (
+      <div>
+        {this.state.menus.map((item, index) => (
+          <CheckableTag
+            key={item}
+            checked={this.state.index == index}
+            onChange={() => this.setState({ index })}
+          >
+            {menuMap[item]}
+          </CheckableTag>
+        ))}
+      </div>
+    )
+
     return (
       <Layout>
-        <div>{ this.props.shh[1].title }</div>
-        <div>{ this.props.shh[1].url }</div>
-
-        <Button size='small' type='primary'>OK</Button>
-        <Button size='small' style={{ marginLeft: 8 }}>Cancel</Button>
+        <List
+          header={sitesMenu}
+          style={{ backgroundColor: '#fff' }}
+          bordered
+          dataSource={this.props[this.state.menus[this.state.index]]}
+          renderItem={(item, index) => (
+            <List.Item>
+              <List.Item.Meta
+                title={
+                  <div>
+                    <span>{`${index + 1}.`}&nbsp;</span>
+                    <a target="_blank" href={item.url} style={{ fontWeight: 400 }}>{item.title}</a>
+                  </div>
+                }
+                description={item.summary}
+              />
+            </List.Item>
+          )}
+        />
         <style jsx>{`
-          div {
-            color: green;
-          }
+          a:link, a:visited { color: #0a0a0a; }
+          a:hover, a:active { color: gray!important; }
         `}</style>
       </Layout>
     )
