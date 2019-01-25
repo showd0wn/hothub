5e1a20f26303385d7b0eeedf9724d363583529ad
Dreamland <showd0wn.land@gmail.com>
2019-01-25 13:04:53 +0800

    up

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,13 @@ const menuMap = {
 
 export default class Index extends Component {
   static async getInitialProps({ query }) {
-    const index = query.tab || 0
     const menus = Object.keys(menuMap)
+    let index
+    if (query && query.tab !== undefined) {
+      index = query.tab
+    } else {
+      index = 0
+    }
     // eslint-disable-next-line no-undef
     const resp = await fetch(`https://api.qingzhiyu.com/news/list?channel=${menus[index]}`)
     const json = await resp.json()
