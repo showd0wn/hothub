6cdaa289149a6290b64c89660e0356626e209236
Yi Feng <dreamland@Yis-MacBook-Pro.local>
2019-10-13 21:48:23 +0800

    up

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,9 @@ export default class Index extends Component {
   static async getInitialProps({ query }) {
     const index = query.tab || 0
     const menus = Object.keys(menuMap)
+    const domain = process.env.NODE_ENV === 'production' ? 'https ://api.qingzhiyu.com' : 'http://localhost:8000'
     // eslint-disable-next-line no-undef
-    const resp = await fetch(`https://api.qingzhiyu.com/news/list?channel=${menus[index]}`)
+    const resp = await fetch(`${domain}/news/list?channel=${menus[index]}`)
     const json = await resp.json()
     return {
       index,
