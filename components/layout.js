51f2031f33adb97917dd301aa34fe8b92e5d693d
fengyi <fengyi24@huawei.com>
2020-07-22 14:12:03 +0800

    meta 优化

--- components/layout.js
+++ components/layout.js
@@ -8,8 +8,8 @@ export default ({ children, title = '卿志宇的关注' }) => (
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content='initial-scale=1.0, width=device-width' />
-      <meta name="keywords" content="卿志宇,热点,资讯,关注,聚合" />
-      <meta name="description" content="热点聚合：知乎、虎扑、豆瓣、NGA、V2EX、Github、SegmentFault" />
+      <meta name="keywords" content="热点,关注,资讯,聚合,卿志宇" />
+      <meta name="description" content="聚合知乎、虎扑、豆瓣、NGA、V2EX、Github、SegmentFault等热点资讯" />
       <link rel="shortcut icon" href="/static/favicon.ico" />
       <title>{ title }</title>
     </Head>
